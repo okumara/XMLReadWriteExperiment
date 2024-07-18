1bd24312ba5fc83192b6cc789bb4eb7d544845e6
oeta <oeta.kumara@gmail.com>
2024-07-18 14:24:14 +0200

    Experiment adding a second library. Key is to note that an XML can only have one root element, so to add a second library node, both library nodes have to be added to a new root element

--- src/main/java/org/geekforgeeks/XMLWriterExample.java
+++ src/main/java/org/geekforgeeks/XMLWriterExample.java
@@ -20,8 +20,11 @@ public class XMLWriterExample {
         Document document = builder.newDocument();
 
         // Create root element
-        Element root = document.createElement("library");
+        Element root = document.createElement("libraries");
         document.appendChild(root);
+
+        Element library = document.createElement("library");
+        root.appendChild(library);
 
         // Create book elements and add text content
         Element book1 = document.createElement("Program1");
@@ -32,10 +35,30 @@ public class XMLWriterExample {
         book3.appendChild(document.createTextNode("JavaScript"));
         Element book4 = document.createElement("Program4");
         book4.appendChild(document.createTextNode("C Programming"));
-        root.appendChild(book1);
-        root.appendChild(book2);
-        root.appendChild(book3);
-        root.appendChild(book4);
+        library.appendChild(book1);
+        library.appendChild(book2);
+        library.appendChild(book3);
+        library.appendChild(book4);
+
+        // Create root element
+        library = document.createElement("library");
+        root.appendChild(library);
+
+        // Create book elements and add text content
+        Element book5 = document.createElement("Program1");
+        book5.appendChild(document.createTextNode("Basic Programming"));
+        Element book6 = document.createElement("Program2");
+        book6.appendChild(document.createTextNode("Pascal Programming"));
+        Element book7 = document.createElement("Program3");
+        book7.appendChild(document.createTextNode("6502 Assemble Programming"));
+        Element book8 = document.createElement("Program4");
+        book8.appendChild(document.createTextNode("Groovy Programming"));
+        library.appendChild(book5);
+        library.appendChild(book6);
+        library.appendChild(book7);
+        library.appendChild(book8);
+
+
 
         // Write to XML file
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
