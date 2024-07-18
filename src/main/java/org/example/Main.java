8d773bbfebb891231709cba4e92b3b4e30ad8312
oeta <oeta.kumara@gmail.com>
2024-07-18 12:36:40 +0200

    Adjustments to make xmlReaderExample runnable.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import static org.geekforgeeks.XMLReaderExample.xmlReaderExample;
 import static org.geekforgeeks.XMLWriterExample.xmlWriterExample;
 
 public class Main {
     public static void main(String[] args) {
         try {
             xmlWriterExample();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            xmlReaderExample();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
