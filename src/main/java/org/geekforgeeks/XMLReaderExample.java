4b6a274734019a6ad8f6e05e708424c80504dc7f
oeta <oeta.kumara@gmail.com>
2024-07-18 12:19:29 +0200

    Copied sources from geek for geeks and adapted for internal use

---
+++ src/main/java/org/geekforgeeks/XMLReaderExample.java
@@ -0,0 +1,31 @@
+package org.geekforgeeks;
+
+// Java Program to Read XML Using DOM Parser
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import java.io.File;
+
+public class XMLReaderExample {
+    public static void xmlReaderExample(String[] args) throws Exception {
+        // Specify the file path as a File object
+        File xmlFile = new File("C:/Users/Mahesh/Desktop/DSAˍPratice/output.xml");
+
+        // Create a DocumentBuilder
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // Parse the XML file
+        Document document = builder.parse(xmlFile);
+
+        // Access elements by tag name
+        NodeList nodeList = document.getElementsByTagName("library");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            System.out.println("Element Content: " + node.getTextContent());
+        }
+    }
+}
